0675aca89ed0061a674b63b0fcaa83f079e2a1d8
Entreprenerd <alex@entreprenerd.xyz>
2022-10-18 01:02:56 +0200

    feat: setup

---
+++ src/Cdp.sol
@@ -0,0 +1,177 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+// NOTE: Solmate doesn't check for token existence, this may cause bugs if you enable any collateral
+import {SafeTransferLib} from "../lib/solmate/src/utils/SafeTransferLib.sol";
+
+import {ERC20} from "../lib/solmate/src/tokens/ERC20.sol";
+
+enum RepayWith {
+    DAI,
+    COLLATERAL
+}
+contract Dai {
+
+    address immutable OWNER;
+
+    mapping(address => uint256) balances;
+
+    constructor() {
+        // TODO: Must deploy from CDP
+        OWNER = msg.sender;
+    }
+
+    function mint(address recipient, uint256 amount) external {
+        balances[recipient] += amount;
+    }
+
+    function balanceOf(address recipient) external view returns (uint256) {
+        return balances[recipient];
+    }
+
+    function burn(address recipient, uint256 amount) external {
+        balances[recipient] -= amount;
+    }
+}
+
+interface ICallbackRecipient {
+
+    function flashMintCallback(address initiator, uint256 amount, bytes memory data) external returns (RepayWith, uint256);
+}
+
+contract Cdp {
+    using SafeTransferLib for ERC20;
+
+    uint256 constant MAX_BPS = 10_000;
+
+    uint256 constant LIQUIDATION_TRESHOLD = 10_000; // 100% in BPS
+
+    Dai immutable public DAI;
+
+    ERC20 immutable public COLLATERAL;
+
+    uint256 totalDeposited;
+
+    uint256 totalBorrowed;
+
+    // Oracle for Ratio
+    uint256 ratio = 3e17; // Conversion rate
+
+    uint256 constant RATIO_DECIMALS = 10 ** 8;
+
+    address owner;
+
+    constructor(ERC20 collateral) {
+        DAI = new Dai();
+        COLLATERAL = collateral;
+    }
+
+    // NOTE: Unsafe should be protected by governance (tbh should be a feed)
+    function setRatio(uint256 newRatio) external {
+        ratio = newRatio;
+    }
+
+    function flash(uint256 amount, ICallbackRecipient target, bytes memory data) external {
+        // No checks as we allow minting after
+
+        // Effetcs
+        uint256 newTotalBorrowed = totalBorrowed + amount;
+
+        totalBorrowed = newTotalBorrowed;
+
+        // Interactions
+        // Flash mint amount
+        // Safe because DAI is nonReentrant as we know impl
+        DAI.mint(target, amount);
+
+
+        // Callback
+        (RepayWith collateralChoice, uint256 repayAmount) = target.flashMintCallback(msg.sender, amount, data);
+
+
+        // Check solvency
+        if(totalBorrowed > maxBorrow()) {
+            if(collateralChoice == RepayWith.DAI) {
+                uint256 minRepay = totalBorrowed - maxBorrow();
+                // They must repay
+                // This is min repayment
+                require(repayAmount >= minRepay);
+
+                // TODO: This may be gameable must fuzz etc.. this is a toy project bruh
+                totalBorrowed -= repayAmount;
+
+                // Get the repayment
+                // DAI Cannot reenter because we know impl, DO NOT ADD HOOKS OR YOU WILL GET REKT
+                DAI.burn(target, repayAmount);
+            } else {
+                // They repay with collateral
+
+                // NOTE: WARN
+                // This can reenter for sure, DO NOT USE IN PROD
+                deposit(repayAmount);
+
+                assert(isSolvent());
+            }
+        }
+    }
+
+
+    // Deposit
+    function deposit(uint256 amount) public {
+        // Increase deposited
+        totalDeposited += amount;
+
+        if (owner == address(0)) {
+            owner = msg.sender;
+        }
+
+        // Check delta + transfer
+        uint256 prevBal = COLLATERAL.balanceOf(address(this));
+        COLLATERAL.safeTransferFrom(msg.sender, address(this), amount);
+        uint256 afterBal = COLLATERAL.balanceOf(address(this));
+
+        // Make sure we got the amount we expected
+        require(afterBal - prevBal == amount, "No feeOnTransfer");   
+    }
+
+    // Borrow
+    function borrow(uint256 amount) external {
+        // Checks
+        uint256 newTotalBorrowed = totalBorrowed + amount;
+        
+        // Check if borrow is solvent
+        uint256 maxBorrowCached = maxBorrow();
+
+        require(newTotalBorrowed <= maxBorrowCached, "Over debt limit");
+
+        // Effect
+        totalBorrowed = newTotalBorrowed;
+
+        // Interaction
+        DAI.mint(msg.sender, amount);
+    }
+
+    function maxBorrow() public view returns (uint256) {
+        return totalDeposited * ratio / RATIO_DECIMALS;
+    }
+
+    function isSolvent() public view returns (bool) {
+        return totalBorrowed <= maxBorrow();
+    }
+
+    // Liquidate
+    function liquidate() external {
+        require(!isSolvent(), "Must be insolvent");
+
+        uint256 excessDebt = totalBorrowed - maxBorrow();
+
+
+        // Give the contract to liquidator
+        owner = msg.sender;
+
+
+        // Burn the token
+        DAI.burn(msg.sender, excessDebt);
+    }
+
+}
--- test/SampleContract.t.sol
+++ test/SampleContract.t.sol
@@ -3,20 +3,26 @@ pragma solidity 0.8.15;
 
 import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
 
-import {SampleContract} from "../src/SampleContract.sol";
+import {Cdp} from "../src/Cdp.sol";
+import {ERC20} from "../lib/solmate/src/tokens/ERC20.sol";
+
+// Useful links
+// How to steal tokens for forknet: 
+// https://github.com/foundry-rs/forge-std/blob/2a2ce3692b8c1523b29de3ec9d961ee9fbbc43a6/src/Test.sol#L118-L150
+// All the basics
+// https://github.com/dabit3/foundry-cheatsheet
+// Foundry manual
+// https://book.getfoundry.sh/cheatcodes/
+
 
 contract SampleContractTest is DSTestPlus {
-    SampleContract sampleContract;
+    Cdp cdpContract;
 
     function setUp() public {
-        sampleContract = new SampleContract();
+        cdpContract = new Cdp();
     }
 
     function testFunc1() public {
-        sampleContract.func1(1337);
-    }
-
-    function testFunc2() public {
-        sampleContract.func2(1337);
+        cdpContract.deposit(1337);
     }
 }
