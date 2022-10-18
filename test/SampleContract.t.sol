2a09bbc3f4eef9ad2cd0194935f1c6c96a7c47cf
Entreprenerd <alex@entreprenerd.xyz>
2022-10-18 19:07:28 +0200

    feat: baby sitting

--- test/SampleContract.t.sol
+++ test/SampleContract.t.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+import "forge-std/Test.sol";
+
 
 import {Cdp} from "../src/Cdp.sol";
 import {ERC20} from "../lib/solmate/src/tokens/ERC20.sol";
@@ -15,13 +16,22 @@ import {ERC20} from "../lib/solmate/src/tokens/ERC20.sol";
 // https://book.getfoundry.sh/cheatcodes/
 
 
-contract SampleContractTest is DSTestPlus {
+contract SampleContractTest is Test {
 
     ERC20 public constant BADGER = ERC20(0x3472A5A71965499acd81997a54BBA8D852C6E53d);
+    
+    // Become this guy
+    address user = address(1);
+
     Cdp cdpContract;
 
     function setUp() public {
         cdpContract = new Cdp(BADGER);
+
+        vm.prank(user);
+
+        // Get a bunch of collateral
+        deal(address(BADGER), user, 123e18);
     }
 
     function testFunc1() public {
